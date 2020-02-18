525b03155f8bee11e480a1bc62b38b4beaa08c0f
Ajith Balakrishnan <32813021+Ajithbalakrishnan@users.noreply.github.com>
2020-02-18 11:32:23 +0530

    Update jetson_imgzmq.py

--- jetson_imgzmq.py
+++ jetson_imgzmq.py
@@ -45,7 +45,8 @@ cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 #cap = VideoStream(gstreamer_pipeline(flip_method=0)).start()
 time.sleep(2.0)
 jpeg_quality = 95
- 
+fps = cap.get(cv2.CAP_PROP_FPS)
+print ("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
 while True:
 	# read the frame from the camera and send it to the server
     ret_val, img = cap.read()
